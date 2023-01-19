19c82c55bf5d65018def041eb59bf75a94d0aaf6
Dani Pereira <pdaniele@outlook.com.br>
2023-01-19 15:38:05 -0300

    Initial commit

--- src/ExercicioDoze/Main.java
+++ src/ExercicioDoze/Main.java
@@ -1,7 +1,17 @@
 package ExercicioDoze;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner leitorScanner = new Scanner(System.in);
+
+        double custoFabrica = leitorScanner.nextDouble();
+
+        double custoConsumidor = ((custoFabrica + (custoFabrica * 0.28)) * 0.45) + (custoFabrica + (custoFabrica * 0.28));
+
+        System.out.println(custoConsumidor);
+;
     }
 }
