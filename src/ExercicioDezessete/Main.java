706f47709911cf51367d75ac1dc2c8a401c7c5ca
Dani Pereira <pdaniele@outlook.com.br>
2023-01-19 18:48:32 -0300

    Atualização de códigos

--- src/ExercicioDezessete/Main.java
+++ src/ExercicioDezessete/Main.java
@@ -1,10 +1,30 @@
 package ExercicioDezessete;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] numeros;
+        Scanner leitorScanner = new Scanner(System.in);
+
+        int numero;
+        List numeros = new ArrayList<>();
+
+
+        for (int i = 0; i<5; i++){
+
+            numero = leitorScanner.nextInt();
+          if (numero >= 10 && numero <=150){
+              numeros.add(numero);
+          }
+
+        }
+
+
+
+        System.out.println(numeros.toArray().length);
 
 
 
