349463ff2f1c766d3a7b18f676a66fc13d591131
Dani Pereira <pdaniele@outlook.com.br>
2023-01-22 14:29:38 -0300

    Atualização de códigos

--- src/ExercicioVinteedois/Main.java
+++ src/ExercicioVinteedois/Main.java
@@ -1,8 +1,61 @@
 package ExercicioVinteedois;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner leitorScanner = new Scanner(System.in);
+        int precoCusto, precoVenda;
+        List <Integer>listaCusto = new ArrayList<>();
+        List <Integer>listaVenda = new ArrayList<>();
+        int somaCusto = 0;
+        int somaVenda = 0;
+
+       // double somaCusto, somaVenda;
+
+        for(int i =0; i<40; i++){
+
+            System.out.println("Informe preço de custo de produto: ");
+            precoCusto = leitorScanner.nextInt();
+            listaCusto.add(precoCusto);
+            System.out.println("Informe o preço de venda do produto: ");
+            precoVenda = leitorScanner.nextInt();
+            listaVenda.add(precoVenda);
+
+            if(precoCusto < precoVenda){
+                System.out.println("Lucro");
+            } else if (precoCusto > precoVenda) {
+                System.out.println("Prejuízo");
+
+            }else {
+                System.out.println("Empate");
+            }
+
+
+        }
+
+        for(int i =0; i<40; i++){
+
+            int p = i+1;
+
+            System.out.println("Produto "+ p +": Valor de custo: "+ listaCusto.get(i) + ", Valor de venda " + listaVenda.get(i));
+        }
+
+        for(int i =0; i<40; i++){
+
+
+          somaCusto =  listaCusto.stream().reduce(Integer::sum).get();
+          somaVenda = listaVenda.stream().reduce(Integer::sum).get();
+        }
+
+        double mediaCusto = somaCusto / listaCusto.toArray().length;
+        double mediaVenda = somaVenda / listaCusto.toArray().length;
+
+        System.out.println("Média de Custo: " + mediaCusto + ", Média de Venda: " + mediaVenda);
 
     }
 }
--- src/ExercicioVinteeum/Main.java
+++ src/ExercicioVinteeum/Main.java
@@ -1,9 +1,50 @@
 package ExercicioVinteeum;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner leitorScanner = new Scanner(System.in);
+
+        String nome, sexo;
+        int idade;
+
+        int qtdPessoas = 1;
+        List apta = new ArrayList<>();
+        List naoApta = new ArrayList<>();
+
+        while (qtdPessoas == 1){
+
+            System.out.println("Informe o nome ou fim para finalizar:");
+            nome = leitorScanner.next();
+
+            if(nome.equals("fim")){
+                break;
+            };
+            System.out.println("Informe a idade:");
+            idade = leitorScanner.nextInt();
+            System.out.println("Informe o sexo:");
+            sexo = leitorScanner.next();
+
+            if(idade>=18 && sexo.equals("masculino")){
+                apta.add(nome);
+                System.out.println("Apta");
+            }else {
+                naoApta.add(nome);
+                System.out.println("Não Apta");
+            }
+
+
+
+
+        }
+
+        System.out.println("Quantidade de pessoas aptas: "+ apta.toArray().length);
+        System.out.println("Quantidade de pessoas não aptas: "+ naoApta.toArray().length);
 
 
     }
